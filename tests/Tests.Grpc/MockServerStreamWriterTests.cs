35a20a4a9b34f811eebf2afcd96cbbd0367d2c7a
Renato Golia <renato.golia@insightarchitectures.com>
2021-05-10 16:47:44 +0200

    Add test double for IServerStreamWriter (#8)

---
+++ tests/Tests.Grpc/MockServerStreamWriterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.Idioms;
+using InsightArchitectures.Testing;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    [TestOf(typeof(MockServerStreamWriter<>))]
+    public class MockServerStreamWriterTests
+    {
+        [Test, CustomAutoData]
+        public void Constructor_does_not_accept_nulls(GuardClauseAssertion assertion) => assertion.Verify(typeof(MockServerStreamWriter<>).GetConstructors());
+
+        [Test, CustomAutoData]
+        public async Task Instance_contains_items_passed_via_WriteAsync(MockServerStreamWriter<HelloReply> sut, HelloReply item)
+        {
+            await sut.WriteAsync(item);
+
+            Assert.That(sut, Contains.Item(item));
+        }
+    }
+}
